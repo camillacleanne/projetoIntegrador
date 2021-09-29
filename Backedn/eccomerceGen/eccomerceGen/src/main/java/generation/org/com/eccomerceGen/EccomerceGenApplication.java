5b1a94e5d6b13e05767ca9b3fafbe0453470657d
camillacleanne <cleanecamilla@gmail.com>
2021-09-29 18:48:54 -0300

    modelo git exemplo para o versionamento do PI

---
+++ Backedn/eccomerceGen/eccomerceGen/src/main/java/generation/org/com/eccomerceGen/EccomerceGenApplication.java
@@ -0,0 +1,13 @@
+package generation.org.com.eccomerceGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccomerceGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EccomerceGenApplication.class, args);
+	}
+
+}
---
+++ Backedn/eccomerceGen/eccomerceGen/src/test/java/generation/org/com/eccomerceGen/EccomerceGenApplicationTests.java
@@ -0,0 +1,13 @@
+package generation.org.com.eccomerceGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EccomerceGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
